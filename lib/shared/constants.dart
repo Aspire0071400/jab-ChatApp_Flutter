69c832cb7d162bcdeac84a53f6c4b296d1bf5be3
Aspire0071400 <prabhujivats@gmail.com>
2022-11-06 19:18:14 +0530

    firebase installed with initialisation code .

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:88151125156:web:0e6810b29d15b9054aca31";
+  static String apiKey = "AIzaSyBVPcXXAcdidju5BWEiyQk6WISWAdY0IOc";
+  static String messagingSenderId = "88151125156";
+  static String projectId = "jabchatappflutter";
+}
